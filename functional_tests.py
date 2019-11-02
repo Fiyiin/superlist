1e41bc898bb96c187ccba658318f885afe023a05
Fiyin Kolawole <kolawolemmanuel@hotmail.com>
2019-11-02 23:17:01 +0100

    Add unittest module

--- functional_tests.py
+++ functional_tests.py
@@ -1,7 +1,24 @@
+import unittest
+
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 
-browser = webdriver.Chrome(ChromeDriverManager().install())
-browser.get('http://localhost:8000')
 
-assert 'Django' in browser.title
+class NewVisitorTest(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Chrome(ChromeDriverManager().install())
+
+
+    def tearDoown(self):
+        self.browser.quit()
+
+
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+
+        self.assertIn('To-do', self.browser.title)
+        self.fail('Flush the test!')
+
+if __name__ == '__main__':
+    unittest.main(warnings='ignore')
